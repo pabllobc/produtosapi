81e054a1092f2feb185f123f5e655d91fa294c27
Pabllo Borges Cardoso <pabllo.cardoso@apoioprodesp.sp.gov.br>
2025-07-09 15:59:43 -0300

    feature: mapeamento jpa

--- src/main/java/io/github/pabllobc/produtosapi/model/Produto.java
+++ src/main/java/io/github/pabllobc/produtosapi/model/Produto.java
@@ -1,10 +1,23 @@
 package io.github.pabllobc.produtosapi.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+// POJO -> Plain Old Java Object
+@Entity
+@Table
 public class Produto {
 
+    @Id
+    @Column(unique = true)
     private int id;
+    @Column
     private String nome;
+    @Column
     private String descricao;
+    @Column
     private double preco;
 
     public int getId() {
