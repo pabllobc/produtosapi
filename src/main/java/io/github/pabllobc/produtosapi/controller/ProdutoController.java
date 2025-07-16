4466d361d9f18c801a62b9baabc94961756643e5
Pabllo Borges Cardoso <pabllo.cardoso@apoioprodesp.sp.gov.br>
2025-07-16 11:00:42 -0300

    feature: produtosapi-final

--- src/main/java/io/github/pabllobc/produtosapi/controller/ProdutoController.java
+++ src/main/java/io/github/pabllobc/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import io.github.pabllobc.produtosapi.model.Produto;
 import io.github.pabllobc.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -29,6 +30,26 @@ public class ProdutoController {
 
     @GetMapping("{id}")
     public Produto obterPorId(@PathVariable ("id") String id){
+
         return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void deletar(@PathVariable ("id") String id) {
+
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void atualizar(@PathVariable ("id") String id,
+                          @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> obterTodos(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
+
     }
 }
